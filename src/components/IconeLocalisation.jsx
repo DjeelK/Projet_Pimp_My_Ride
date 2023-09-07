667e2fe74832a9e4c81a3512adf8e77082e82f2f
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-07 16:59:14 +0200

    création composant icone ok. utilisation props pour flexibilite. création bouton composant en cours

--- src/components/IconeLocalisation.jsx
+++ src/components/IconeLocalisation.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import '../css/IconeLocalisation.css';
 
-const IconeLocalisation = () => {
+const IconeLocalisation = (props) => {
   return (
     <div className="icon-container">
       <div className="icon-wrapper">
-        <svg
-          width="40"
-          height="40"
-          viewBox="0 0 40 40"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#ffffff" /* Couleur de l'icône en blanc */
-        >
-          {/* Insérez ici le code SVG de votre logo */}
-          <circle cx="20" cy="20" r="15" />
-        </svg>
+        <img
+          src={props.iconeLocalisation}
+          alt="Localisation"
+          width="40" 
+          height="40" 
+        />
       </div>
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import '../css/SearchBar.css'
 import IconeLocalisation from './IconeLocalisation';
+import LocalisationIcon from '../assets/location-2955.png'; 
+import CalendrierIcon from '../assets/vecteezy_calendar-icon-sign-symbol-design_10160301_960.png';
+
 
 const SearchBar = () => {
   return (
     <div className="container col-12">
       <div className="row justify-content-center align-items-center vh-100 col-12">
         <div className="rounded p-3 text-light text-center" id="bar_search">
-          <IconeLocalisation/>
+          <div class="row">
+          <div class="col">
+          <IconeLocalisation iconeLocalisation = {LocalisationIcon} />
+          </div>
+          <div class="col">
+          <IconeLocalisation iconeLocalisation = {LocalisationIcon} />
+          </div>
+          <div class="col">
+          <IconeLocalisation iconeLocalisation = {CalendrierIcon} />
+          </div>
+          </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
