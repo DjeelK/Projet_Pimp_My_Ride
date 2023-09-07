0f63e9bad48c61f314fcd4b72e66578d9f0b0b28
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-07 23:36:46 +0200

    bis

--- src/components/FooterPage.jsx
+++ src/components/FooterPage.jsx
@@ -8,10 +8,11 @@ const FooterPage = () => {
       </div>
       <div className="center-content-nb1">
         <h1 className="large-text">50K</h1>
-        <div className="small-text">Utilisateurs</div>
+        <div className="small-text-nb1">Utilisateurs</div>
       </div>
       <div className="right-content-nb2">
         <h1 className="right-h1">500K</h1> 
+        <div className="small-text-nb2">Km parcourus</div>
       </div>
     </div>
   );
