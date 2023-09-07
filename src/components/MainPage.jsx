44451416509dd3bd29c8db8c6f1ae9681d845852
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-07 15:11:55 +0200

    bis

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/MainPage.css';
+import SearchBar from './SearchBar';
 
 const MainPage = () => {
   return (
@@ -9,6 +10,7 @@ const MainPage = () => {
           <h1 className="title_appli">Welcome To Pimp My Ride</h1>
           <h4 className="description_appli"> Prêt(e) à rejoindre la ligue des covoiturés intrépides ? Alors fais ta réservation et embarque avec nous !</h4>
         </div>
+        <SearchBar/>
       </div>
     </div>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return(
+        <div className="container">
+        <div className="my-3 row">
+          <div className="col-10 offset-1 rounded bg-dark p-3 text-light">
+            <h1>Coucou</h1>
+          </div>
+        </div>
+        </div>
+    )
+}
+
+export default SearchBar;
