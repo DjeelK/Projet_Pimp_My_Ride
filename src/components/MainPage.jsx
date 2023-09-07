0b59ad79454dab34bfb8b63858d8953a7ef75d0e
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-08 00:02:36 +0200

    add gif fumée popopop

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -10,6 +10,7 @@ const MainPage = () => {
           <h1 className="title_appli">Welcome To Pimp My Ride</h1>
           <h4 className="description_appli"> Prêt(e) à rejoindre la ligue des covoiturés intrépides ? Alors fais ta réservation et embarque avec nous !</h4>
           <SearchBar/>
+          <img src="../assets/1525580_4263d.gif" alt="Animation de fumée" className="smoke" />
         </div>
       </div>
     </div>
