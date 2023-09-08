25ffce33eee4d2a22d29946cf16f853f2ace7f41
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-08 22:40:44 +0200

    Fumée position animation ok

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/MainPage.css';
 import SearchBar from './SearchBar';
+import SmokeGif from '../assets/fumee.gif';
 
 const MainPage = () => {
   return (
@@ -10,7 +11,7 @@ const MainPage = () => {
           <h1 className="title_appli">Welcome To Pimp My Ride</h1>
           <h4 className="description_appli"> Prêt(e) à rejoindre la ligue des covoiturés intrépides ? Alors fais ta réservation et embarque avec nous !</h4>
           <SearchBar/>
-          <img src="../assets/1525580_4263d.gif" alt="Animation de fumée" className="smoke" />
+          <img src={SmokeGif} alt="Smoke" className="smoke" />
         </div>
       </div>
     </div>
