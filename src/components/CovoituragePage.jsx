78952fb8ab807010b28ea83afdb5ce991f1fb05a
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-10 15:27:34 +0200

    bis

--- src/components/CovoituragePage.jsx
+++ src/components/CovoituragePage.jsx
@@ -8,14 +8,15 @@ const CovoituragePage = () => {
     <div>
       <NavigationBar />
       <div className="container-fluid-covoit">
-          <div className="main-content-covoit col-6">
-            <SearchBar />
+        <div className="main-content-covoit col-6">
+          <SearchBar />
+          <div className="overlay-rectangle">
+            
           </div>
         </div>
       </div>
-  
+    </div>
   );
 };
 
 export default CovoituragePage;
-
