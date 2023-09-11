91c5a2f215d8ca46e9496a49ffeb4007317db467
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-09-11 23:07:20 +0200

    bis

--- src/components/TrajetsPage.jsx
+++ src/components/TrajetsPage.jsx
@@ -1,16 +1,28 @@
 import TripForm from "../routes/trip/TripForm";
 import NavigationBar from "./NavigationBar";
 import "../css/Trip.css";
-
+import fondAddTrajet from "../assets/fond_add_trajet_2.jpg";
+ 
 const TripPage = () => {
-    return(
-        <div>
-            <NavigationBar/>
-                <div className="container-add-trajet">
-                    <TripForm/>
-                </div>
+  return (
+    <div>
+      <NavigationBar />
+      <div className="container" style={{ marginTop: '50px' }}>
+        <div className="row">
+          <div>
+            <img
+              src={fondAddTrajet}
+              alt="Image de fond"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-md-7">
+            <TripForm />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default TripPage;
